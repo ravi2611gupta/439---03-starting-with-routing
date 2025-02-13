7aea3a881e7ea32424e69d3b5cfe69b821cc07d6
ravi <ravi2611gupta@gmail.com>
2025-02-13 13:08:55 +0530

    440 videos done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-
-import { WelcomeComponent } from './welcome/welcome.component';
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
   standalone: true,
-  imports: [WelcomeComponent],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  imports: [RouterModule],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {}
