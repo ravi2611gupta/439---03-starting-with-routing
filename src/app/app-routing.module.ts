990690987ab261d08e3f5d587f06c9adc958e6a3
ravi <ravi2611gupta@gmail.com>
2025-02-13 13:23:44 +0530

    441 vidoes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Route, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
+import { NgModule } from "@angular/core";
+import { Route, RouterModule } from "@angular/router";
+import { AboutComponent } from "./about/about.component";
 
-import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeComponent } from "./welcome/welcome.component";
 
 const routes: Route[] = [
   {
-    path: '',
+    path: "",
     component: WelcomeComponent,
   },
   {
-    path: 'about',
-    component: AboutComponent,
+    path: "about",
+    // component: AboutComponent, // ! This will work for eager loading for both case (standalone: true | false). But if we want to use lazy loading in the standalone component then we need to use the loadComponent instead of loadChildren.
+    loadComponent: () =>
+      import("./about/about.component").then((comp) => comp.AboutComponent),
   },
   {
-    path: 'dashboard',
+    path: "dashboard",
     loadChildren: () =>
-      import('./dashboard/dashboard-routing.module').then(
-        (mod) => mod.DashboardRoutingModule
-      ),
+      import("./dashboard/routes").then((routes) => routes.DASHBOARD_ROUTE),
   },
 ];
 
